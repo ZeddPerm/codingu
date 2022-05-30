0de307d14ad9fee9fc31a3ab7adfc6b69d81c662
ZeddPerm <jamesbdhoang@gmail.com>
2022-05-29 17:23:14 -0700

    Update tictactoe.py

--- Neural Networks/reinforcementlearning/tictactoe.py
+++ Neural Networks/reinforcementlearning/tictactoe.py
@@ -1,6 +1,8 @@
 import itertools
+import copy
 import numpy as np
 import time
+import random
 gamestate = [['','',''],
             ['','',''],
             ['','','']]
@@ -12,6 +14,8 @@ all_gamestates = []
 
 aiX = []
 aiO = []
+
+epsilon = 0.3
 
 def resultOfGame(gs):
     if gs[0][0] == gs[0][1] == gs[0][2]:
@@ -32,50 +36,65 @@ def resultOfGame(gs):
         return gs[0][0]
     if '' not in gs[0] and '' not in gs[1] and '' not in gs[2]:
         return 'draw'
-print('a',resultOfGame([['', '', 'x'], ['', '', 'x'], ['', 'x', '']]))
+    print('not done')
+    return 'not done'
+# print('a',resultOfGame([['', '', 'x'], ['', '', 'x'], ['', 'x', '']]))
 def bestPossibleMove(gs,cur_player):
+    global epsilon
     open_spaces = []
     possible_values = []
     for i in range(len(gs)):
         for j in range(len(gs[i])):
             if gs[i][j] == '':
-                gs[i][j] = cur_player
+                temp_gs = copy.deepcopy(gs)
+                temp_gs[i][j] = cur_player
                 open_spaces.append([i,j])
-                print(gs)
-                if cur_player == 'x':
-                    # print(type(all_gamestates))
-                    # print('a',all_gamestates.index(gs))
-                    if gs in all_gamestates:
-                        # print(len(aiX))
-                        # print(aiX[all_gamestates.index(gs)])
-                        possible_values.append(aiX[all_gamestates.index(gs)])
-    print(open_spaces,possible_values)
+                # print(temp_gs)
+                # print(gs)
+                if temp_gs in all_gamestates:
+                    if cur_player == 'x':
+                        possible_values.append(aiX[all_gamestates.index(temp_gs)])
+                    elif cur_player == 'o':
+                        possible_values.append(aiO[all_gamestates.index(temp_gs)])
+    # print(open_spaces,possible_values)
+    v = random.random()
+    if v > epsilon:
+        return open_spaces[np.argmax(possible_values)]
+    else:
+        epsilon -= 0.01
+        return open_spaces[random.randint(0,len(open_spaces)-1)]
     # return open_spaces
 
 for i in all_gamestate:
     i = [list(i[0:3]),list(i[3:6]),list(i[6:9])]
     all_gamestates.append(i)
-    if resultOfGame(i) == 'x':
-        # print(i,all_gamestates[all_gamestates.index(i)])
-        # time.sleep(1)
+    result = resultOfGame(i)
+    if result == 'x':
         aiX.append(1)
         aiO.append(-1)
-    elif resultOfGame(i) == 'o':
-        # print(i,all_gamestates[all_gamestates.index(i)])
-        # time.sleep(1)
+    elif result == 'o':
         aiO.append(1)
         aiX.append(-1)
-    elif resultOfGame(i) == 'draw' or resultOfGame(i) == '':
-        # print(i,all_gamestates[all_gamestates.index(i)])
-        # time.sleep(1)
+    else:
         aiO.append(0)
         aiX.append(0)
-
 value_of_gamestate = {}
 
-# print(all_gamestates)
-# print('aegh',all_gamestates.index(aiX[1][1]))
-# print('aegh',aiX[1])
-# print(list(all_gamestate))
-# print(all_gamestates[0])
-print(bestPossibleMove([['x','','x'],['x','o','o'],['x','o','x']], 'x'))
+# print('ag',len(all_gamestates))
+# print('x',len(aiX))
+# print('o',len(aiO))
+# print(bestPossibleMove([['x','','o'],
+#                         ['o','',''],
+#                         ['x','o','o']], 'o'))
+results = 0
+player = players[random.randint(0,1)]
+while results != 10:
+    move = bestPossibleMove(gamestate,player)
+    gamestate[move[0]][move[1]] = player
+    results += 1
+    print(resultOfGame(gamestate))
+    if player == 'x':
+        player = 'o'
+    elif player == 'o':
+        player = 'x'
+    print('g',gamestate)
