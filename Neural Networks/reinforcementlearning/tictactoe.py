602261d9bc86da9a0b934f63911c987254b1d796
ZeddPerm <jamesbdhoang@gmail.com>
2022-06-03 18:44:25 -0700

    ncjkdszn

--- Neural Networks/reinforcementlearning/tictactoe.py
+++ Neural Networks/reinforcementlearning/tictactoe.py
@@ -18,25 +18,25 @@ aiO = []
 epsilon = 0.3
 
 def resultOfGame(gs):
-    if gs[0][0] == gs[0][1] == gs[0][2]:
-        return gs[0][0]
-    if gs[1][0] == gs[1][1] == gs[1][2]:
-        return gs[1][0]
-    if gs[2][0] == gs[2][1] == gs[2][2]:
-        return gs[2][0]
-    if gs[0][0] == gs[1][1] == gs[2][2]:
-        return gs[0][0]
-    if gs[0][1] == gs[1][1] == gs[2][1]:
-        return gs[0][1]
-    if gs[0][2] == gs[1][2] == gs[2][2]:
-        return gs[0][2]
-    if gs[0][2] == gs[1][1] == gs[2][0]:
-        return gs[0][2]
-    if gs[0][0] == gs[1][0] == gs[2][0]:
-        return gs[0][0]
+    if gs[0][0] == gs[0][1] == gs[0][2] != "":
+        return gs[0][0],"done"
+    if gs[1][0] == gs[1][1] == gs[1][2] != "":
+        return gs[1][0],"done"
+    if gs[2][0] == gs[2][1] == gs[2][2] != "":
+        return gs[2][0],"done"
+    if gs[0][0] == gs[1][1] == gs[2][2] != "":
+        return gs[0][0],"done"
+    if gs[0][1] == gs[1][1] == gs[2][1] != "":
+        return gs[0][1],"done"
+    if gs[0][2] == gs[1][2] == gs[2][2] != "":
+        return gs[0][2],"done"
+    if gs[0][2] == gs[1][1] == gs[2][0] != "":
+        return gs[0][2],"done"
+    if gs[0][0] == gs[1][0] == gs[2][0] != "":
+        return gs[0][0],"done"
     if '' not in gs[0] and '' not in gs[1] and '' not in gs[2]:
-        return 'draw'
-    return 'not done'
+        return None,'draw'
+    return None,'not done'
 # print('a',resultOfGame([['', '', 'x'], ['', '', 'x'], ['', 'x', '']]))
 def bestPossibleMove(gs,cur_player):
     global epsilon
@@ -55,7 +55,6 @@ def bestPossibleMove(gs,cur_player):
                         possible_values.append(aiX[all_gamestates.index(temp_gs)])
                     elif cur_player == 'o':
                         possible_values.append(aiO[all_gamestates.index(temp_gs)])
-    # print(open_spaces,possible_values)
     v = random.random()
     if v > epsilon:
         return open_spaces[np.argmax(possible_values)]
@@ -67,7 +66,7 @@ def bestPossibleMove(gs,cur_player):
 for i in all_gamestate:
     i = [list(i[0:3]),list(i[3:6]),list(i[6:9])]
     all_gamestates.append(i)
-    result = resultOfGame(i)
+    result,_ = resultOfGame(i)
     if result == 'x':
         aiX.append(1)
         aiO.append(-1)
@@ -88,13 +87,41 @@ value_of_gamestate = {}
 results = 0
 player = players[random.randint(0,1)]
 learning_rate = 0.2
-while results != 'not done':
-    move = bestPossibleMove(gamestate,player)
-    gamestate[move[0]][move[1]] = player
-    results = resultOfGame(gamestate)
-    
-    if player == 'x':
-        player = 'o'
-    elif player == 'o':
-        player = 'x'
-    print('g',gamestate)
+# aiX = np.loadtxt("aiX.txt", dtype=np.float64)
+# aiO = np.loadtxt("aiO.txt", dtype=np.float64)
+for i in range(1000):
+    gamestate = [['','',''],
+                ['','',''],
+                ['','','']]
+    results = 'not done'
+    player = players[random.randint(0,1)]
+    learning_rate = 0.2
+    while results == 'not done':
+        if player == 'x':
+            x_state = aiX[all_gamestates.index(gamestate)]
+            # print(aiX[all_gamestates.index(gamestate)])
+            x_state_gs = copy.deepcopy(gamestate)
+            move = bestPossibleMove(gamestate,player)
+            gamestate[move[0]][move[1]] = player
+            _,results = resultOfGame(gamestate)
+            new_x_state = aiX[all_gamestates.index(gamestate)]
+            x_state += learning_rate*(new_x_state - x_state)
+            aiX[all_gamestates.index(x_state_gs)] = x_state
+            player = 'o'
+            # print(aiX[all_gamestates.index(x_state_gs)])
+        elif player == 'o':
+            o_state = aiO[all_gamestates.index(gamestate)]
+            # print(aiO[all_gamestates.index(gamestate)])
+            o_state_gs = copy.deepcopy(gamestate)
+            move = bestPossibleMove(gamestate,player)
+            gamestate[move[0]][move[1]] = player
+            _,results = resultOfGame(gamestate)
+            new_o_state = aiO[all_gamestates.index(gamestate)]
+            o_state += learning_rate*(new_o_state - o_state)
+            aiO[all_gamestates.index(o_state_gs)] = o_state
+            # print(aiO[all_gamestates.index(gamestate)])
+            player = 'x'
+        # print('g',gamestate)
+        # print('r',results)
+np.savetxt("aiX.txt", aiX, fmt="%.4f")
+np.savetxt("aiO.txt", aiO, fmt="%.4f")
